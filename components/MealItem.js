38904be7ff0b28c320b59612f010d4acaae0583a
Hassan <hassan.abbascs@gmail.com>
2020-11-11 12:32:09 +0200

    feat: render meals list based on category

---
+++ components/MealItem.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	ImageBackground,
+} from 'react-native';
+
+const MealItem = (props) => {
+	return (
+		<View style={styles.mealItem}>
+			<TouchableOpacity onPress={props.onSelectMeal}>
+				<View>
+					<View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+						<ImageBackground
+							source={{ uri: props.image }}
+							style={styles.bgImage}
+						>
+							<View style={styles.titleContainer}>
+								<Text style={styles.title} numberOfLines={1}>
+									{props.title}
+								</Text>
+							</View>
+						</ImageBackground>
+					</View>
+					<View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+						<Text>{props.duration}m</Text>
+						<Text>{props.complexity}</Text>
+						<Text>{props.affordability}</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	mealItem: {
+		height: 180,
+		width: '100%',
+		backgroundColor: '#f5f5f5',
+		borderRadius: 10,
+		overflow: 'hidden',
+	},
+
+	bgImage: {
+		width: '100%',
+		height: '100%',
+		justifyContent: 'flex-end',
+	},
+
+	titleContainer: {
+		backgroundColor: 'rgba(0,0,0,0.5)',
+		paddingVertical: 3,
+		paddingHorizontal: 12,
+	},
+
+	title: {
+		fontFamily: 'open-sans-bold',
+		fontSize: 19,
+		textAlign: 'center',
+		color: 'white',
+	},
+
+	mealRow: {
+		flexDirection: 'row',
+	},
+
+	mealHeader: {
+		height: '85%',
+	},
+
+	mealDetail: {
+		height: '15%',
+		paddingHorizontal: 10,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+});
+
+export default MealItem;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = (props) => {
+	const renderMealItem = (itemData) => {
+		return (
+			<MealItem
+				title={itemData.item.title}
+				image={itemData.item.imageUrl}
+				duration={itemData.item.duration}
+				complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+				onSelectMeal={() => {}}
+			/>
+		);
+	};
+
 	const id = props.navigation.getParam('categoryId');
 
-	const selectedCategory = CATEGORIES.find((cat) => cat.id === id);
+	const displayedMeals = MEALS.filter(
+		(meal) => meal.categoryIds.indexOf(id) >= 0
+	);
 
 	return (
 		<View style={styles.screen}>
-			<Text>{selectedCategory.title}</Text>
-			<Button
-				title="Go to meal details"
-				onPress={() => {
-					props.navigation.navigate({
-						routeName: 'MealDetails',
-					});
-				}}
+			<FlatList
+				data={displayedMeals}
+				renderItem={renderMealItem}
+				style={{ width: '95%' }}
 			/>
 		</View>
 	);
