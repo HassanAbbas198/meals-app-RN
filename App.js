53ef31403d43fc082de582a929ba7125a48c56db
Hassan <hassan.abbascs@gmail.com>
2020-11-11 08:42:28 +0200

    feat: install & add navigation to the app

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
+import MealsNavigator from './navigation/MealsNavigator';
+
+const fetchFonts = () => {
+	return Font.loadAsync({
+		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+	});
+};
 
 export default function App() {
-	return <View></View>;
+	const [fontLoaded, setFontLoaded] = useState(false);
+
+	if (!fontLoaded) {
+		return (
+			<AppLoading
+				startAsync={fetchFonts}
+				onFinish={() => setFontLoaded(true)}
+			/>
+		);
+	}
+
+	return <MealsNavigator />;
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: '100%',
+	},
+});
